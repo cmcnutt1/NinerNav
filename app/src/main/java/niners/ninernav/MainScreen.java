6375449545926480802f1ecea1da1aa3296ff3e6
Chris McNutt <cmcnutt1@uncc.edu>
2016-10-04 11:54:15 -0400

    Added a few screens and made skeletons for them for sprint 1. Only settings button and Navigation button from home screen are functional.

--- app/src/main/java/niners/ninernav/MainScreen.java
+++ app/src/main/java/niners/ninernav/MainScreen.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainScreen extends AppCompatActivity {
@@ -16,7 +15,7 @@ public class MainScreen extends AppCompatActivity {
 
 
         //Button Listener for Navigation Button
-        ImageButton navButton = (ImageButton)findViewById(R.id.navButton);
+        ImageButton navButton = (ImageButton)findViewById(R.id.mapButton);
         assert navButton != null;
         navButton.setOnClickListener(new View.OnClickListener(){
             @Override
--- app/src/main/java/niners/ninernav/NavScreen.java
+++ app/src/main/java/niners/ninernav/NavScreen.java
@@ -3,11 +3,11 @@ package niners.ninernav;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SettingsScreen extends AppCompatActivity {
+public class NavScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings_screen);
+        setContentView(R.layout.activity_nav_screen);
     }
 }
